194a946b0a1a1832d1f710ca63e1e00a4db9306b
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-02-16 16:11:59 +0900

    learn: 구체 클래스 기반 프록시 - 적용

--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,10 +1,8 @@
 package hello.proxy.app.v2;
 
-import hello.proxy.app.v2.OrderServiceV2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
